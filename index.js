6b11a1a464ca194c0fa083415912e64f9b4af21a
Aniket Patel <aniketupatel1@gmail.com>
2019-02-13 01:32:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(kittens) {
-  kittens.push("Ralph");
+  kittens.push('Ralph');
   return kittens;
 }
 
