9d03993446952b563d052333717008f7d6be5945
Aniket Patel <aniketupatel1@gmail.com>
2019-02-13 02:00:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(name) {
   return [name, ...kittens];
 }
 
-function removeLastKitten() {
+function removeLastKitten(kittens) {
   kittens = kittens.slice(0,kittens.length-1);
   return kittens;
 }
